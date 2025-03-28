a9d04f5c5003b2e6a502d632849cbe8756f711a6
eyiqian <1286532715@qq.>
2025-03-28 15:08:39 +0800

    111

--- src/main/java/com/magicshop/MagicShop.java
+++ src/main/java/com/magicshop/MagicShop.java
@@ -16,7 +16,7 @@ public class MagicShop {
     private static final Logger LOGGER = LogUtils.getLogger();
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,MODID);
     public static final RegistryObject<Item> magic_shop = ITEMS.register("magicshop",()->new Item(new Item.Properties()));
-
+//1212112121212121
     public MagicShop() {
         var bus = FMLJavaModLoadingContext.get().getModEventBus();
         ITEMS.register(bus);
